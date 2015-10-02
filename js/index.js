8c40344ef8cedd768b33bb068c5d80893ccf6220
Justin Ying <just10ying@gmail.com>
2015-10-01 20:37:26 -0400

    Splash Improvement
    Better on mobile

--- js/index.js
+++ js/index.js
@@ -1,12 +1,6 @@
 $(document).ready(function() {
-    // skrollr has poor performance on mobile, so don't enable it on mobile devices:
-    // Thanks to: http://stackoverflow.com/a/10364620/5356439
-    var isMobile = window.matchMedia("only screen and (max-width: 760px)");
-    if (!isMobile.matches) {
-        var s = skrollr.init();
-    }
     setTimeout(function() {
-        $('.jumbotron-text.container.unselectable').fadeIn(1000);
+        $('.jumbotron-text.container').fadeIn(1000);
     }, 250);
 });
 
@@ -15,3 +9,55 @@ function scrollToElement(element) {
         scrollTop: $(element).offset().top
     }, 650);
 }
+
+
+/* Angular JS */
+
+var experienceApp = angular.module('experienceApp', []);
+experienceApp.controller('experienceController', function($scope) {
+    var epic2015 = new Object();
+    epic2015.title = "Epic";
+    epic2015.imgSrc = "resources/epic.jpg";
+    epic2015.position = "Android Development Intern";
+    epic2015.description = "I worked at Epic!  It was cool.  I did things.";
+    epic2015.bullets = [
+        "Created a multi-layer image annotation tool for Android, which supports drawing, text, and distance annotations",
+        "Utilized OpenCV for reference object detection, allowing for relative distance measurements",
+        "Designed tool resolution to change dynamically to save memory without quality loss"
+    ];
+    
+    var tedResearch = new Object();
+    tedResearch.title = "University of Pittsburgh: Thermoelectric Device Simulation";
+    tedResearch.imgSrc = "resources/benedum.jpg";
+    tedResearch.position = "Research Assistant";
+    tedResearch.description = "Ray tracing!";
+    tedResearch.bullets = [
+        "Created a multi-layer image annotation tool for Android, which supports drawing, text, and distance annotations",
+        "Utilized OpenCV for reference object detection, allowing for relative distance measurements",
+        "Designed tool resolution to change dynamically to save memory without quality loss"
+    ];
+    
+    var epic2014 = new Object();
+    epic2014.title = "Epic";
+    epic2014.imgSrc = "resources/hyperspace.jpg";
+    epic2014.position = "Hyperspace Web Development Intern";
+    epic2014.description = "Epic hwyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy";
+    epic2014.bullets = [
+        "Created a multi-layer image annotation tool for Android, which supports drawing, text, and distance annotations",
+        "Utilized OpenCV for reference object detection, allowing for relative distance measurements",
+        "Designed tool resolution to change dynamically to save memory without quality loss"
+    ];
+    
+    var bentley = new Object();
+    bentley.title = "Bentley Systems";
+    bentley.imgSrc = "resources/bentley.png";
+    bentley.position = "Software Development Intern";
+    bentley.description = "Enums hwyyyyyyyyyyyyyyyyyyyyy";
+    bentley.bullets = [
+        "Created a multi-layer image annotation tool for Android, which supports drawing, text, and distance annotations",
+        "Utilized OpenCV for reference object detection, allowing for relative distance measurements",
+        "Designed tool resolution to change dynamically to save memory without quality loss"
+    ];
+    
+    $scope.expList = [epic2015, tedResearch, epic2014, bentley];
+});
